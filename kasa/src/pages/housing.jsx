dc948f8cca4aff65479cf7a254dac079d39d547b
Clément Théraud <theraud.clement@gmail.com>
2022-05-31 13:07:38 +0200

    scss + variables

--- kasa/src/pages/housing.jsx
+++ kasa/src/pages/housing.jsx
@@ -9,10 +9,40 @@
 //import
 
 function Housing() {
+    //recup id
     const {id} = 
-    const {}
+    //recup loc
+   // const {}
 }
 
 return (
-    <div className=""
+    <div className="housing">
+        <div className="picture">
+            //carousel
+        </div>
+        <div className="content">
+            <div className="head">
+                <div className="title">
+                        <h1>
+                            {title}
+                        </h1>
+                        <h3>
+                            {location}
+                        </h3>
+                        <div className="tags">
+                            //tags
+                        </div>
+                </div>
+                <div className="rates">
+                    //rates stars
+                </div>
+            </div>
+            <dir className="textes">
+                //description
+                //equipements
+            </dir>
+        </div>
+    </div>
 )
+
+export default Housing;
