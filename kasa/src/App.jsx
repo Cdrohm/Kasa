6e6ecf29d1a937b79024f964478869267a238cb2
Clément Théraud <theraud.clement@gmail.com>
2022-05-31 13:25:03 +0200

    app modif
    import gestion

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -1,3 +1,12 @@
+import React from 'react';
+
+import Header from './components/header';
+import Footer from './components/footer';
+import Housing from './pages/housing';
+import about from './pages/about';
+import Error from './pages/error'
+import Home from './pages/home';
+
 import logo from './logo.svg';
 import './App.css';
 
