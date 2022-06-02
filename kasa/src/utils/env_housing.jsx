31cf167002ea5ded555e230caf7c77716a785ccb
Clément Théraud <theraud.clement@gmail.com>
2022-06-02 15:22:51 +0200

    const lessor and search housing fetch

--- kasa/src/utils/env_housing.jsx
+++ kasa/src/utils/env_housing.jsx
@@ -2,3 +2,7 @@ import { createContext, useState } from "react";
 //import housing by fetch?
 
 export const envHousing = createContext();
+
+export const lessor = ({ children }) => {
+    const [locs, setLocs ] = useState(housingfetch.get());
+}
