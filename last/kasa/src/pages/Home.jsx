5b572fa11d8b04263e265acdcd5f0bd642a13345
Clément Théraud <theraud.clement@gmail.com>
2022-06-13 14:09:59 +0200

    sass

--- last/kasa/src/pages/Home.jsx
+++ last/kasa/src/pages/Home.jsx
@@ -1,31 +1,16 @@
 import React from 'react';
-import { useContext } from 'react';
-import { ContextHousing } from '../utils/context_Housing';
-
-import Photo from "../components/modules/Picture";
-import Grid from "../components/modules/Grid";
 
 const Home = () => {
-	const { locs, getIDs } = useContext(ContextHousing);
-	const locIDs = getIDs();
-	if (
-		!localStorage.getItem("locIDs") ||
-		localStorage.getItem("locIDs") === undefined ||
-		localStorage.getItem("locIDs") === []
-	) {
-		localStorage.setItem("locIDs", JSON.stringify(locIDs));
-	}
-
 	return (
-		<div> 
-			<Photo hpImg={true} />0
-			<Grid locs={locs} />
+		<div>
+			Hey
 		</div>
-	
 	);
 };
 
 export default Home;
+
+
 /*import Photo from "../components/modules/Picture";
 import Grid from "../components/modules/Grid";
 
